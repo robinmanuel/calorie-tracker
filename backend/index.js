db2c0f58db8a806e361d03efc4adaeaef96bc6ee
robinmanuel <robinreginald21@gmail.com>
2026-03-12 16:13:17 +0530

    configure database and cors for deployment

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const recordRoutes = require("./routes/records");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/records", recordRoutes);
